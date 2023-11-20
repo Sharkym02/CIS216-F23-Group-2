34aa11060a92da60c30e58c919f6ed200fcf12cc
unknown <cj75423@mail.harpercollege.edu>
2023-11-20 15:44:10 -0600

    Added a MenuBar and split the Tkinter's elements into individual classes. Additionally, the canvas now fills in the window.

--- main.py
+++ main.py
@@ -4,15 +4,40 @@ from tkinter import ttk, TclError, filedialog, font
 #from tkinter.scrolledtext import ScrolledText as Text
 #from tkinter.colorchooser import askcolor
 
+class GameCanvas(Canvas):
+	def __init__(self, root, *args, **kwargs):
+		# https://tkinter-docs.readthedocs.io/en/latest/widgets/canvas.html
+		Canvas.__init__(self, root, *args, **kwargs)
+		# self._root = root
+		self.configure(bg="green")
+
+		self.create_text(20, 30, anchor=W, font="Ubuntu",
+            text="This is canvas drawing")
+		
+		return
+
+class MenuBar(Menu):
+	def __init__(self, root, *args, **kwargs):
+		Menu.__init__(self, root, *args, **kwargs)
+		# self._root = root
+		gameMenu = Menu(self, tearoff=0)
+
+		gameMenu.add_command(label="New Game")
+		# Additional buttons could be: Undo, Redo (?), Scores
+
+		self.add_cascade(label="Game", menu = gameMenu)
+
+		root.config(menu=self)
+		return
+
+
 if __name__ == "__main__":
 	root = Tk()
 	#frame = ttk.Frame(root,padding=10)
-	
-	#https://tkinter-docs.readthedocs.io/en/latest/widgets/canvas.html
-	canvas = Canvas(root, bg="green", height=480, width=640)
-	canvas.create_text(20, 30, anchor=W, font="Ubuntu",
-            text="This is canvas drawing")
-	canvas.pack()
+
+	canvas = GameCanvas(root)
+	menuBar = MenuBar(root)
+	canvas.pack(expand=True, fill="both")
 	
 	root.geometry("640x480")
 	root.mainloop()
