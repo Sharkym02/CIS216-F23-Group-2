a504012836828018894b102d8006d1a6f91b285c
Sharkym02 <35713039+Sharkym02@users.noreply.github.com>
2023-11-21 18:13:22 -0600

    updated game logic

--- gamelogic.py
+++ gamelogic.py
@@ -1,22 +1,46 @@
+import random
+
 class Card:
 	def __init__(self, value:int, suit:int, faceUp:bool):
 		# spade: 0, heart: 1, club: 2, diamond: 3
 		self.value = value
 		self.suit = suit
 		self.faceUp = faceUp
-		
-class Column:
-	def __init__(self, location:int, stack:Card):
-		self.location = location
-		self.stack = stack
+
 	
 class CardManager:
-    def __init__(self, deck):
-        self.deck = deck
+	# The end of the list is the top of the deck
+	# The beginning of the list is the bottom of the deck
+	def __init__(self, deck: list):
+		self.deck = deck
         
-    def createDeck(self):
+	def createDeck(self):
     # make a deck with each card in it
-        for x in range(1, 14):
-            for y in range(4):
-                self.deck[x+13*(y)] = Card(x, y, 0)
+		for x in range(1, 14):
+			for y in range(4):
+				self.deck[x+13*(y)] = Card(x, y, 0)
+
+	def reset(self):
+		self.deck.clear()
+		self.createDeck()
                 
+	def popTop(self):
+		last = self.deck.pop(-1)
+		return last
+	
+	def popTop(self):
+		first = self.deck.pop(0)
+		return first
+	
+	def addTop(self, card: Card):
+		self.deck.append(card)
+
+	def addBottom(self, card: Card):
+		self.deck.insert(card)
+
+	def shuffle(self):
+		random.shuffle(self.deck)
+
+class Column(CardManager):
+	def __init__(self, location, *args, **kwargs):
+		Column.__init__(self, location, *args, **kwargs)
